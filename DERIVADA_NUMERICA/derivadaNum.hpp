a394568fecd94f26bbaa8a887cd35cc807ea4372
aluiziodev <aluneto2000@gmail.com>
2026-03-05 11:47:29 -0300

    implementaçao derivada num com controle de erro

--- DERIVADA_NUMERICA/derivadaNum.hpp
+++ DERIVADA_NUMERICA/derivadaNum.hpp
@@ -1,9 +1,12 @@
 #ifndef DERIVADA_NUM_HPP
 #define DERIVADA_NUM_HPP
-#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <algorithm>
 
 float f(float const x){
-    return sin(x)*pow(x, 3)*5;
+    return sin(x)*powf(x, 3)*5;
 }
 
 float dfNum1_FW(float const x, float const dlt){
@@ -18,7 +21,6 @@ float dfNum1_CT(float const x, float const dlt){
     return (f(x+dlt)-f(x-dlt))/2*dlt;
 }
 
-
 float dfNum2_FW(float const x, float const dlt){
     return (f(x+2*dlt)-2*f(x+dlt)-f(x))/dlt*dlt;
 }
@@ -30,6 +32,49 @@ float dfNum2_BW(float const x, float const dlt){
 float dfNum2_CT(float const x, float const dlt){
     return (f(x+dlt)-2*f(x)+f(x-dlt))/dlt*dlt;
 }
+
+float dfNum3_FW(float const x, float const dlt){
+    return (f(x+3*dlt)-3*f(x+2*dlt)+3*f(x+dlt)-f(x))/powf(dlt, 3);
+}
+
+float dfNum3_BW(float const x, float const dlt){
+    return (f(x)-3*f(x-dlt)+3*f(x-2*dlt)-f(x-3*dlt))/powf(dlt, 3);
+}
+
+float dfNum3_CT(float const x, float const dlt){
+    return (f(x+2*dlt)-2*f(x+dlt)+2*f(x-dlt)-f(x-2*dlt))/8*powf(dlt, 3);
+}
+
+//DERIVADA NUMERICA COM CONTROLE DE ERRO
+
+
+
+float dfNum_SqrError(float const x, float const dlt){
+    return (-f(x+2*dlt)+4*f(x+dlt)-3*f(x))/2*dlt;
+} // Erro da ordem quadratica, Calc do erro dado por: f'''(x)*dlt*dlt/3  O(dlt^2)
+
+float dfNum_CubcError(float const x, float const dlt){
+    return (f(x+3*dlt)/9-f(x+2*dlt)/2+f(x+dlt)-f(x)*5/9)*3/dlt;
+} // Erro da ordem quadratica, Calc do erro dado por: -f^iv(x)*1/4*dlt^3/3  O(dlt^3)
+
+
+
+
+/*float dfNumN_FW(float const x, float const dlt, int const n){
+    if (n==0) return f(x);
+    else return (dfNumN_FW(x+dlt, dlt, n-1)-dfNumN_FW(x, dlt, n-1))/dlt;
+}
+
+float dfNumN_BW(float const x, float const dlt, int const n){
+    if (n==0) return f(x);
+    else return (dfNumN_FW(x, dlt, n-1)-dfNumN_FW(x-dlt, dlt, n-1))/dlt;
+}
+
+float dfNumN_CT(float const x, float const dlt, int const n){
+    if (n==0) return f(x);
+    else return (dfNumN_FW(x+dlt, dlt, n-1)-dfNumN_FW(x-dlt, dlt, n-1))/2*dlt;
+}*/
+
 
 
 
